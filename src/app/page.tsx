ba283950aa069d6f1bc803b0f1e9a4a79d25522f
nATOr <gowluonade@gmail.com>
2023-03-15 03:22:08 -0500

    ul li

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,18 @@ import api from '@/api'
 export default async function Home () {
   const ballots = await api.ballot.list()
   return (
-    <main><pre>{JSON.stringify(ballots, null, 2)}</pre></main>
+    <main className='container mx-auto mt-10'>
+      {/* <pre>{JSON.stringify(ballots, null, 2)}</pre> */}
+      {ballots.map((ballot) => (
+        <section key={ballot.id}>
+          <h3 className='font-bold text-3xl'>{ballot.id}</h3>
+          <ul>
+            {ballot.items.map(nominee => (
+              <li key={nominee.id}>{nominee.title}</li>
+            ))}
+          </ul>
+        </section>
+      ))}
+    </main>
   )
 }
